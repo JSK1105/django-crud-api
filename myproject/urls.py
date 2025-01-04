59fd7199482511b8ddfabab79831e07302167683
JYOTHI SAI KIRAN NVN <jyothisaikiran11@gmail.com>
2025-01-04 22:11:02 +0530

    Add root URL for the homepage

--- myproject/urls.py
+++ myproject/urls.py
@@ -22,8 +22,13 @@ urlpatterns = [
 ]
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("Welcome to the Django CRUD API!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),  # Include API routes
+    path('', home),  # Add this line for the root URL
 ]
